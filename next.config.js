b08f713a6a11b6e320e81f0169b088e6ca3b857c
zelinasw <zelinaaswa@gmail.com>
2026-03-02 21:20:06 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true }
+  images: {
+    unoptimized: true,
+  },
+  // INI JURUSNYA: Memaksa Cloudflare mengenali rute halaman
+  trailingSlash: true,
+  output: 'export', 
 };
+
 module.exports = nextConfig;
